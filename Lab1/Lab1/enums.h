3f00a97d53cf99c5dfd5956c718b4a2ac4a2df5d
Michael Liu <liu.michael@wustl.edu>
2019-02-11 17:36:16 -0600

    done grading

--- Lab1/Lab1/enums.h
+++ Lab1/Lab1/enums.h
@@ -1,4 +1,4 @@
 #pragma once
 
 enum errors {Success, TooManyArguments, CorrectNumberOfArguments, UnableToOpen};
-enum file {programName, inputFile};
+enum file {programName, inputFile}; // move CorrectNumberOfArguments to the file enum -ML
